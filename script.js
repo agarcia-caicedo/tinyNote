dca50f0749d7ce050ad7433819c40aafd61aa774
Ana Garcia <agarcia-caicedo@myseneca.ca>
2019-09-12 19:37:55 -0400

    added save and download capabilities

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $( ".inner-switch" ).on("click", function() {
     if( $( "body" ).hasClass( "light" )) {
       $( "body" ).removeClass( "light" );
-      $( ".inner-switch" ).text( "OFF" );
+      $( ".inner-switch" ).text( "Light Mode" );
     } else {
       $( "body" ).addClass( "light" );
-      $( ".inner-switch" ).text( "ON" );
+      $( ".inner-switch" ).text( "Dark Mode" );
     }
 });
 
@@ -17,3 +17,30 @@ bkLib.onDomLoaded(function() {
     myNicEditor.addInstance('note');
 });
 
+const fs = new Filer.FileSystem();
+window.addEventListener('DOMContentLoaded', (event) => {
+    fs.readFile('/note', 'utf8', function (err, data) {
+        if (err) {
+            document.querySelector("#note").innerHTML = "Welcome to my notepad! Start Typing...";
+        }
+        else if (data) {
+            document.querySelector("#note").innerHTML = data;
+        }
+    });
+});
+var intervalID = window.setInterval(save, 20000);
+function save(a) {
+    fs.writeFile('/note', document.querySelector('#note').innerHTML);
+    console.log(a);
+
+}
+
+function saveAlert(){
+    save();
+    alert("Saved!");
+}
+
+function download(){
+    var blob = new Blob([document.querySelector('#note').innerHTML], {type: "text/xml;charset=utf-8"});
+    saveAs(blob, "tiny.xml");
+}
