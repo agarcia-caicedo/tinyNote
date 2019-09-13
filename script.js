19a5b01f1c26badb3f905659ce5658cb51b1fa74
Khang Nguyen <nnknguyen@myseneca.ca>
2019-09-13 00:51:49 -0400

    add  Quill rich text editor

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-$( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "light" )) {
-      $( "body" ).removeClass( "light" );
-      $( ".inner-switch" ).text( "Light Mode" );
-    } else {
-      $( "body" ).addClass( "light" );
-      $( ".inner-switch" ).text( "Dark Mode" );
-    }
+var quill = new Quill('#note', {
+    modules: {
+        toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+        ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
 });
 
 const fs = new Filer.FileSystem();
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    fs.readFile('/note', 'utf8', function (err, data) {
-        if (err) {
-            document.querySelector("#note").innerHTML = "Welcome to my notepad! Start Typing...";
-        }
-        else if (data) {
-            document.querySelector("#note").innerHTML = data;
+    $( ".inner-switch" ).on("click", function() {
+        if( $( "body" ).hasClass( "light" )) {
+          $( "body" ).removeClass( "light" );
+          $( ".inner-switch" ).text( "Light Mode" );
+        } else {
+          $( "body" ).addClass( "light" );
+          $( ".inner-switch" ).text( "Dark Mode" );
         }
     });
 
-});
-var intervalID = window.setInterval(save, 20000);
-function save(a) {
-    fs.writeFile('/note', document.querySelector('#note').innerHTML);
-    console.log(a);
+    fs.readFile('/note', 'utf8', function (err, data) {
+        if (err) {
+            quill.setText('Welcome to my note! Start typing...');
+        }
+        else if (data) {
+            quill.setContents(JSON.parse(data));
+        }
+    });
 
+    var intervalID = window.setInterval(save, 20000);
+
+});
+
+function save() {
+    fs.writeFile('/note', document.querySelector('#note').innerHTML);
 }
 
 function saveAlert(){
@@ -37,15 +49,3 @@ function download(){
     var blob = new Blob([document.querySelector('#note').innerHTML], {type: "text/xml;charset=utf-8"});
     saveAs(blob, "tiny.xml");
 }
-
-var quill = new Quill('#note', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['code-block']
-      ]
-    },
-    placeholder: 'Compose an epic...',
-    theme: 'snow'
-  });
