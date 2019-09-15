0060f0b7b3f0183feaf6cb6559127b9db894ce76
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-09-15 12:15:14 -0400

    Introduces PDF printing feature
    Signed-off-by: Timofei Shchepkin <dgrz5.0@gmail.com>

--- script.js
+++ script.js
@@ -61,3 +61,13 @@ function download(){
     var blob = new Blob([document.querySelector('#con #note').innerHTML], {type: "text/xml;charset=utf-8"});
     saveAs(blob, "tiny.xml");
 }
+
+function downloadPDF(){
+    var doc = new jsPDF();
+    var note = `
+               Agarcia Caicedo's note: ${document.querySelector('#con #note').innerText} \n
+              ~
+              `;
+    doc.text(note, 10, 10);
+    doc.save('tinyNote.pdf')
+}
